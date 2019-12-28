4a31c37ba9db23c00f90a1c2c0f53803c00e248e
Santripta Sharma <santripta@gmail.com>
2019-12-29 01:53:38 +0530

    Make landing proto

---
+++ src/components/main.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+class main extends React.Component
+{
+    render()
+    {
+        return(
+        <div id = "main">
+            <div id = "who">
+                <div className = "content">
+                    <h1>Hello, I'm Santript<span className="strike">a</span></h1>
+                    <br></br>
+                        <p>a 16-year old developer, inept designer and tech enthusiast<br></br><br></br>i enjoy indie games<br></br>i like impromptu debates<br></br>i partake in procrastination</p>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default main;
